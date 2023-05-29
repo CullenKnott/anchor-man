0431a374fe89467b9e3882b18e5c5f6dc1837785
Cullen Knott <cullenp.knott@gmail.com>
2023-05-29 15:18:26 -0700

    added search history feature and removed modal

--- assets/script.js
+++ assets/script.js
@@ -8,16 +8,46 @@ var windEl = document.querySelector('#main-city-wind');
 var locationName = document.querySelector('#main-city-name');
 var forecastCardEl = document.querySelector('#five-day');
 var historyEl = document.querySelector('#search-history');
+var searchList = [] // an array that gets added to the local storage to added subsequent inputs into
+                    // local storage without overwriting the previous input
+
+init()
+
+function init() {
+    var storedSearch = JSON.parse(localStorage.getItem('searchList'));
+    if (storedSearch !== null) {
+        searchList = storedSearch;
+    }
+
+    renderHistory()
+}
+
+function storeHistory() {
+    localStorage.setItem('searchList',JSON.stringify(searchList))
+}
+
+function renderHistory() {
+    for (var i = 0; i < searchList.length; i++) {
+        var search = searchList[i];
 
 
 
 
+    }
 
-// fetch data from API url
+
+    if (!search) {
+        return;
+    } else {
+        callWeather(search)
+        callForecast(search)
+    };
+}
 
 
-//
-function apiCall(search) {
+
+// fetch data from API url for forecast
+function callForecast(search) {
 
     var key = 'd49b1f6a58f01678d1f0b70e468590c6';
     var queryURL =
@@ -27,15 +57,40 @@ function apiCall(search) {
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayForecast(data, search)
-                })
+                    console.log(data);
+                    console.log(data.city.name);
+                    console.log(data.list[0].main.temp)
+                    displayForecast(data,search);
+                });
             } else {
-                alert('Invalid Input')
+                alert('Invalid Input');
             }
         })
   };
 
- 
+
+// fetch data for weather of current day
+function callWeather(search) {
+    var key = 'd49b1f6a58f01678d1f0b70e468590c6';
+    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=" + key;
+
+    fetch(weatherURL)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+            })
+        }
+    })
+}
+
+var displayWeather = function (search,) {
+
+}
+
+var displayForecast = function () {
+    
+}
 
 
 
@@ -48,10 +103,12 @@ function apiCall(search) {
 
 searchBtn.addEventListener('click', function(event) {
     event.preventDefault();
-    var search = locationInput.value.trim();
+    var search = locationInput.value.trim()
 
-    if (search) {
-        apiCall(search)
-    }
-
+    if (search === '') {
+        return
+    } 
+    searchList.push(search);
+    storeHistory();
+    renderHistory();
 });
