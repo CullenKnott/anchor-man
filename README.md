1785c97007c47900469c81e22eebf6b3717a5ffa
Cullen Knott <cullenp.knott@gmail.com>
2023-06-08 16:24:23 -0700

    added interactive search history and appended 5-day forecast

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# anchor-man
-Simple weather application that displays the weather of cities chosen by the user
+# Anchor-man
+
+## Description
+
+Anchor-man is a simple, easy to use weather application that displays the current weather, 5 day forecast, displays your search history
+
+## Table of Contents
+
+
+- [Technology](#Technology)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Technology
+
+-HTML
+
+-CSS
+
+-JavaScript
+
+-Bootstrap
+
+-API (https://openweathermap.org/)
+
+## Usage
+
+Deployment Link: https://cullenknott.github.io/anchor-man/
+
+![Image of Deployed Application](./assets/AnchorMan.png)
+
+## Resources
+
+
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [CullenKnott]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Badges
+
+![badmath](https://img.shields.io/badge/Project%20-Anchor--Man-brightgreen)
+
+## Features
+
+-Search: Users can enter a city name to search for the weather.
+
+-Results Limit: The website displays up to 5 days for the forecast.
+
+-Weather Details: Each card displays the date, an image relating to the type of weather, temperature, humidity, and wind speed.
+
+-Responsive Design: The website is optimized for various devices, ensuring a seamless user experience across desktop and mobile devices.
+
+-Accessibility Features
--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,23 @@ var windEl = document.querySelector('#main-city-wind');
 var locationName = document.querySelector('#main-city-name');
 var forecastCardEl = document.querySelector('#five-day');
 var historyEl = document.querySelector('#search-history');
+var counter = 1
 var searchList = [] // an array that gets added to the local storage to added subsequent inputs into
                     // local storage without overwriting the previous input
+
+function timeDate(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    var year = date.getFullYear()
+    
+    var dayOutput =
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day + '/' + 
+        year;
+    return dayOutput;
+}
 
 init()
 
@@ -27,14 +42,19 @@ function storeHistory() {
 }
 
 function renderHistory() {
+    historyEl.innerHTML = ''
+
     for (var i = 0; i < searchList.length; i++) {
         var search = searchList[i];
 
-
-
-
+        var listHistory = document.createElement('li');
+        listHistory.classList = 'list-group-item';
+        listHistory.setAttribute('id', `list${counter}`);
+        listHistory.setAttribute('data-search', search)
+        listHistory.textContent = search
+        historyEl.appendChild(listHistory);
+        counter++
     }
-
 
     if (!search) {
         return;
@@ -60,7 +80,7 @@ function callForecast(search) {
                     console.log(data);
                     console.log(data.city.name);
                     console.log(data.list[0].main.temp)
-                    displayForecast(data,search);
+                    displayForecast(data);
                 });
             } else {
                 alert('Invalid Input');
@@ -71,6 +91,7 @@ function callForecast(search) {
 
 // fetch data for weather of current day
 function callWeather(search) {
+
     var key = 'd49b1f6a58f01678d1f0b70e468590c6';
     var weatherURL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=" + key;
 
@@ -79,27 +100,76 @@ function callWeather(search) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data)
+                displayWeather(data)
             })
         }
     })
 }
 
-var displayWeather = function (search,) {
+var displayWeather = function (data) {
+    locationName.innerHTML = ''
+    tempEl.innerHTML = ''
+    humidityEl.innerHTML = ''
+    windEl.innerHTML = ''
 
+    var weatherType = data.weather[0].main
+    var img = ''
+    if (weatherType === "Clouds"){
+        img = '☁️'
+    } else if (weatherType === "Clear") {
+        img ='☀️'
+    } else if (weatherType === "Rain") {
+        img ='🌧️'
+    }
+
+    locationName.textContent = data.name + ' ' + timeDate() + ' ' + img// add date mm/dd/yyyy
+    tempEl.textContent = 'Temperature: ' + data.main.temp + ' °F'
+    humidityEl.textContent = 'Humidity: ' + data.main.humidity + '%'
+    windEl.textContent = 'Wind Speed: ' + data.wind.speed + ' mph'
 }
 
-var displayForecast = function () {
-    
+var displayForecast = function (data) {
+    forecastCardEl.innerHTML = ''
+    for(var i=0, j=0; j<=6; i=i+6){
+        var getDate = data.list[i].dt;
+        if(data.list[i].dt != data.list[i+1].dt) {
+            var d = new Date(0);
+            d.setUTCSeconds(getDate)
+            var date = d
+            console.log(date)
+            var month = date.getMonth()+1;
+            var day = date.getDate();
+            var year = date.getFullYear();
+            var cardDate = month + '/' + day + '/' + year;
+            var weatherType = data.list[i].weather[0].main;
+            var image = ''
+            if (weatherType === "Clouds"){
+                image = '☁️'
+            } else if (weatherType === "Clear") {
+                image ='☀️'
+            } else if (weatherType === "Rain") {
+                image ='🌧️'
+            }
+
+            var temperature = data.list[i].main.temp
+            var humidity = data.list[i].main.humidity
+            var windSpeed = data.list[i].wind.speed
+            
+            var weatherCard = 
+    `<div class="card m-2" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${cardDate}</h5>
+            <p>${image}</p>
+            <p class="card-text">Temperature: ${temperature} °F</p>
+            <p class="card-text">Humidity: ${humidity}%</p>
+            <p class="card-text">Wind Speed: ${windSpeed} Mph</p>
+        </div>
+    </div>`
+    forecastCardEl.innerHTML += weatherCard
+    j++
+        }
+    }
 }
-
-
-
-// create event listener that saves input and appends to "search history"
-
-// fetch lat and long based on name of location to attain weather data
-
-// append data to dashboard and five day weather forecast cards
-
 
 searchBtn.addEventListener('click', function(event) {
     event.preventDefault();
@@ -112,3 +182,10 @@ searchBtn.addEventListener('click', function(event) {
     storeHistory();
     renderHistory();
 });
+
+historyEl.addEventListener("click", function(event){
+    if(event.target && event.target.nodeName == 'LI'){
+        callForecast(event.target.dataset.search)
+        callWeather(event.target.dataset.search)
+    }
+})
